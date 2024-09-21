ebf99e7e80d365d589e82d018c6610b7d3088794
jeffszo <jefersonferreira27@outlook.com>
2024-09-21 14:21:57 -0300

    type modal

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -1,34 +1,29 @@
-'use client'
+"use client";
 
 import { useEffect } from "react";
-import styles from './modal.module.scss'
+import styles from "./modal.module.scss";
 import { IModalProps } from "@/app/models/IModal";
 
+export default function Modal({ isOpen, onClose, children }: IModalProps) {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("body-blur");
+    } else {
+      document.body.classList.remove("body-blur");
+    }
 
+    return () => {
+      document.body.classList.remove("body-blur");
+    };
+  }, [isOpen]);
 
-export default function Modal ({isOpen, onClose, children}: ModalProps) {
+  if (!isOpen) return null;
 
-    useEffect(() => {
-
-        if (isOpen) {
-          document.body.classList.add('body-blur');
-        } else {
-          document.body.classList.remove('body-blur');
-        }
-    
-        return () => {
-          document.body.classList.remove('body-blur');
-        };
-
-      }, [isOpen]);
-    
-      if (!isOpen) return null;
-
-    return (
-        <div className={styles.modalOverlay} onClick={onClose}>
+  return (
+    <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
-    )
+  );
 }
